8648a4662c22b1e4826b1f84692a1ab78fc2fd49
jidibinlin <2694273649@qq.com>
2022-09-08 22:00:16 +0800

    大致完成read-frame

--- cl-frame.lisp
+++ cl-frame.lisp
@@ -42,74 +42,126 @@
     )))
 
 (defmethod read-sequence-atlist (sequence stream &key start end)
-  (when (or (< (length sequence) start)
-            (< (length sequence) end)
-            (minusp start)
-            (minusp end)
-            (minusp (- end start)))
-    type-error)
+  (let ((position (read-sequence sequence stream :start start :end end)))
+    (when (< position end)
+      (setf start position)
+      (read-sequence-atlist sequence stream :start position :end end)))
   )
 
 (defmethod read-frame ((codec length-field-based-frame-codec))
   ;;read date from stream stored in the `codec' and decode it againest the codec
   ;;`CLOSED-STREAM-ERROR' will be thrown if the stream is closed, you should handler this err by yourself
-  (let* ((decoder-config (decoder-config codec))
-         (byte-order (dec-length-field-length decoder-config))
-         (lf-length (dec-length-field-length decoder-config))
-         (lf-adjustment (dec-length-adjustment decoder-config))
-         (initial-bytes-to-strip (dec-initial-bytes-to-strip decoder-config))
+  (let* ((byte-order (dec-length-field-length (dec-length-field-length codec)))
+         (lf-offset (dec-length-field-offset (dec-length-field-length codec)))
+         (lf-length (dec-length-field-length (dec-length-field-length codec)))
+         (lf-adjustment (dec-length-adjustment (dec-length-field-length codec)))
+         (initial-bytes-to-strip (dec-initial-bytes-to-strip (dec-length-field-length codec)))
+         (header)
+         (msg)
          )
+    (when (> lf-offset 0)
+      (setf header (make-array lf-offset :element-type '(unsigned-byte 8)))
+      (read-sequence header (iostream codec) :start 0 :end lf-offset)
+      )
+    (multiple-value-bind (lenbuf msg-len) (get-unadjusted-frame-length codec)
+      (setf msg-len (+ msg-len lf-adjustment))
+      (setf msg (make-array msg-len :element-type '(unsigned-byte 8) :initial-element 0))
+      (read-sequence msg (iostream codec) :start 0 :end msg-len)
 
+      (concatenate 'vector header lenbuf msg)
+      )
     )
   )
+
+(defmethod get-unadjusted-frame-length ((codec length-field-based-frame-codec))
+  (let* ((dec-config (decoder-config codec))
+         (len-buf)
+         (length))
+    (case (dec-length-field-length dec-config)
+      (1 (progn
+           (setf len-buf (make-array 1 :element-type '(unsigned-byte 8) :initial-element 0))
+           (read-sequence len-buf (iostream codec) :start 0 :end 1)
+           (length (bit-smasher:int<- len-buf)))
+       (values len-buf length))
+
+      (2 (progn
+           (setf len-buf (make-array 2 :element-type '(unsigned-byte 8) :initial-element 0))
+           (read-sequence len-buf (iostream codec) :start 0 :end 2)
+           (length (bit-smasher:int<- len-buf)))
+       (values len-buf length))
+
+      (3 (progn
+           (setf len-buf (make-array 3 :element-type '(unsigned-byte 8) :initial-element 0))
+           (read-sequence len-buf (iostream codec) :start 0 :end 3)
+           (length (bit-smasher:int<- len-buf)))
+       (values len-buf length))
+
+      (4 (progn
+           (setf len-buf (make-array 4 :element-type '(unsigned-byte 8) :initial-element 0))
+           (read-sequence len-buf (iostream codec) :start 0 :end 4)
+           (length (bit-smasher:int<- len-buf)))
+       (values len-buf length))
+
+      (8 (progn
+           (setf len-buf (make-array 8 :element-type '(unsigned-byte 8) :initial-element 0))
+           (read-sequence len-buf (iostream codec) :start 0 :end 8)
+           (length (bit-smasher:int<- len-buf)))
+       (values len-buf length))
+      )
+    )
+  )
+
+
+(define-condition too-large-length-error (error) ())
+(define-condition minus-length-adjustment-too-small-error (error) ())
 
 (defmethod write-frame ((codec length-field-based-frame-codec) buf)
   ;; encode the buf with length header and write to the socket stream stored in the `codec'
   ;; `CLOSED-STREAM-ERROR' will be thrown if the stream is closed, you should handler this err by yourself
-  (let* ((encoder-config (encoder-config codec))
-         ;; order of the byte
-         (byte-order (enc-byte-order encoder-config))
+  ;; `TOO-LARGE-LENGTH' will be thrown if width of  length-field can`t hold the length
+  ;; `MINUS-LENGTH-ADJUSTMENT-TOO-SMALL-ERROR' will be thrown if minused length-adjustment make the encoded buf length minus
+  (let* (;; order of the byte
+         (byte-order (enc-byte-order (encoder-config codec)))
          ;; length of the length field
-         (lf-length (enc-length-field-length encoder-config))
+         (lf-length (enc-length-field-length (encoder-config codec)))
          ;; length to be add to the length field
-         (lf-adjustment (enc-length-adjustment encoder-config))
+         (lf-adjustment (enc-length-adjustment (encoder-config codec)))
          ;; length includes the length field length
          (length (+ (length buf) lf-adjustment))
          (length-buf)
          )
-	(when (enc-length-includes-length-field-length encoder-config)
+	(when (enc-length-includes-length-field-length enc-config)
       (setf length (+ length lf-length)))
 
     (when (<= length 0)
-      (return-from write-frame (values nil (error "errTooLessLength"))))
+      (error 'minus-length-adjustment-too-small))
     (case lf-length
-          (1 (progn
-               (when (>= length 256)
-                 (error "length does not fit into one byte"))
-               (setf length-buf (make-array 1 :element-type '(unsigned-byte 8) :initial-element 0))
-               (setf (aref length-buf 0) (aref (bit-smasher:octets<- length) 0))))
-          (2 (progn
-               (when (>= length 65536)
-                 (error "length does not fit into a short integer"))
-               (setf length-buf (make-array 2 :element-type '(unsigned-byte 8) :initial-element 0))
-               (put-uint16 byter-order length-buf length)))
-          (3 (progn
-               (when (>= length 16777216)
-                 (error "length does not fit into a medium integer"))
-               (setf length-buf (make-array 3 :element-type '(unsigned-byte 8) :initial-element 0))
-               (put-uint24 byte-order length-buf length)))
-          (4 (progn
-               (when (>= length 4294967296)
-                 (error "length does not fit into a long integer"))
-               (setf length-buf (make-array 4 :element-type '(unsigned-byte 8) :initial-element 0))
-               (put-uint32 byte-order length-buf length)))
-          (8 (progn
-               (when (>= length 18446744073709551616)
-                 (error "length does not fit into a long long integer"))
-               (setf length-buf (make-array 8 :element-type '(unsigned-byte 8) :initial-element 0))
-               (put-uint64 byte-order length-buf length)))
-          )
-    (write-sequence (concatenate 'vector length-buf buf) (iostream codec))
-    (force-output (iostream codec))
-    )
+      (1 (progn
+           (when (>= length 256)
+             (error 'too-large-length)
+             (setf length-buf (make-array 1 :element-type '(unsigned-byte 8) :initial-element 0))
+             (setf (aref length-buf 0) (aref (bit-smasher:octets<- length) 0))))
+       (2 (progn
+            (when (>= length 65536)
+              (error 'too-large-length))
+            (setf length-buf (make-array 2 :element-type '(unsigned-byte 8) :initial-element 0))
+            (put-uint16 byter-order length-buf length)))
+       (3 (progn
+            (when (>= length 16777216)
+              (error 'too-large-length)
+              (setf length-buf (make-array 3 :element-type '(unsigned-byte 8) :initial-element 0))
+              (put-uint24 byte-order length-buf length))))
+       (4 (progn
+            (when (>= length 4294967296)
+              (error 'too-large-length)
+              (setf length-buf (make-array 4 :element-type '(unsigned-byte 8) :initial-element 0))
+              (put-uint32 byte-order length-buf length))))
+       (8 (progn
+            (when (>= length 18446744073709551616)
+              (error 'too-large-length))
+            (setf length-buf (make-array 8 :element-type '(unsigned-byte 8) :initial-element 0))
+            (put-uint64 byte-order length-buf length)))
+       )
+      (write-sequence (concatenate 'vector length-buf buf) (iostream codec))
+      (force-output (iostream codec))))
   )
